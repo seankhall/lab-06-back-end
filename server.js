6aaee2815d5a7c3a9115bbfa4d4314ead26a0d75
RAN VAKNIN <ranvaknin0@gmail.com>
2019-11-05 12:05:32 -0800

    finished weather

--- server.js
+++ server.js
@@ -1,17 +1,46 @@
 'use strict';
 
-require('dotenv').config()
+// require('dotenv').config()
 const express = require('express');
 const cors = require('cors')
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3020;
 
-app.get('/mitchell', (req,res)=>{
-    res.send('JERICA MITCHELL IS IN DA HOUSE!!')
+
+app.get('/location', (request, response) => {
+  const geoData = require('./data/geo.json');
+  const city = request.query.data;
+  const locationData = new Location(city,geoData);
+  response.send(locationData);
 });
 
+app.get('/weather', (req,res) =>{
+  const darkSky = require('./data/darksky.json');
+  const	city = req.query.data;
+  let arrayOfDays = [];
+  for (let i = 0 ; i < 8 ; i++){
+    const summary = darkSky.daily.data[i].summary;
+    const time = darkSky.daily.data[i].time;
+    const weatherData = new Weather(city,summary,time);
+    arrayOfDays.push(weatherData);
+  }
+  res.send(arrayOfDays);
+})
 
-app.listen(PORT, () =>{
-    console.log(`listening on port: ${PORT}.`)
+function Location(city,geoData){
+  this.request_query = city;
+  this.formatted_query = geoData.results[0].formatted_address;
+  this.latitude = geoData.results[0].geometry.location.lat;
+  this.longtitude = geoData.results[0].geometry.location.lng;
+}
+
+function Weather(city,summary,time){
+  this.request_query = city;
+  this.summary = summary;
+  this.time = new Date(time*1000).toGMTString();
+}
+
+app.listen(PORT, () => {
+  console.log(`listening on port: ${PORT}.`)
 });
